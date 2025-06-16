94bba4f1b3a41906403408ab62cb9ce5c130a829
pnm <null>
2025-06-16 11:46:24 +0400

    Add UserDetails for authentification (upload user from db)

---
+++ src/main/java/com/photoalbum/config/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.photoalbum.config;
+
+import com.photoalbum.model.User;
+import com.photoalbum.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        
+        return new CustomUserDetails(
+            user.getUsername(),
+            user.getPassword(),
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")),
+            user.getId()
+        );
+    }
+}
