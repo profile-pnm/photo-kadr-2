f0707b9a69448aff0da34e5cfca9ba4a202e97c6
pnm <null>
2025-06-16 17:18:10 +0400

    Include method for create sql-dumb-file

--- src/main/java/com/photoalbum/controller/MainController.java
+++ src/main/java/com/photoalbum/controller/MainController.java
@@ -3,10 +3,13 @@ package com.photoalbum.controller;
 import com.photoalbum.config.CustomUserDetails;
 import com.photoalbum.model.Photo;
 import com.photoalbum.service.PhotoService;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
@@ -16,6 +19,25 @@ public class MainController {
     
     public MainController(PhotoService photoService) {
         this.photoService = photoService;
+    }
+    
+    // для получения дампа базы данных
+    @RestController
+    public class DbExportController {
+        
+        private final JdbcTemplate jdbcTemplate;
+        
+        public DbExportController(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
+        
+        // ввести localhost:8080/export-db и в корне проекта будет сохранен дамп
+        @GetMapping("/export-db")
+        public String exportDb() throws Exception {
+            String exportPath = "./h2_export.sql";
+            jdbcTemplate.execute("SCRIPT TO '" + exportPath + "'");
+            return "DB exported to: " + exportPath;
+        }
     }
     
     @GetMapping("/")
